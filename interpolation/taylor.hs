5da17647557892e9c93c9d5858e471d9435db5a9
Max Orok <morok@mevex.com>
2019-01-20 20:08:41 -0500

    get rid of main fn dup

--- interpolation/taylor.hs
+++
@@ -1,7 +0,0 @@
-import Data.Scientific as Scientific -- arbitrary precision floats
-import Derivative -- hand-rolled derivative approximation
-
-main :: IO ()
-main = do
-        putStrLn("Taylor interpolation demo")
-
